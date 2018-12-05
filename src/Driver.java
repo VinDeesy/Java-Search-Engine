ac3b8176842afd67205ee2829efdd36e1f95d758
David Hutchinson <dhutchinson@usfca.edu>
2018-12-04 21:28:01 -0800

    easy breezy covergirl

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,4 @@
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -15,32 +12,12 @@ public class Driver {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) {
-		/*
-		 * TODO ArgumentParser parser = new ArgumentParser(args);
-		 * 
-		 * ThreadedInvertedIndex threadedInvertedIndex; InvertedIndex invertedIndex;
-		 * 
-		 * QueryFileParserInterface queryParser;
-		 * 
-		 * if -threads { threadedInvertedIndex = new ThreadedInvertedIndex();
-		 * invertedIndex = threadedInvertedIndex;
-		 * 
-		 * queryParser = new ThreadSafeQueryFileParser(threadedInvertedIndex, threads);
-		 * } else { invertedIndex = new InvertedIndex(); queryParser = new
-		 * QueryParser(invertedIndex); }
-		 * 
-		 * if -path { if (threadedInvertedIndex != null) { call the threaded version }
-		 * else { single threaded } }
-		 * 
-		 * if (-locations) { invertedIndex.... }
-		 * 
-		 */
 
 		if (args.length == 0) {
 			return;
 		}
 		ArgumentParser parser = new ArgumentParser();
-
+		parser.parse(args);
 		ThreadedInvertedIndex threadedInvertedIndex = null;
 		InvertedIndex index;
 		QueryFileParserInterface queryParser;
@@ -85,7 +62,7 @@ public class Driver {
 		if (parser.hasFlag("-index")) {
 
 			Path outputPath = parser.getPath("-index", Paths.get("index.json"));
-			try (BufferedWriter writer = Files.newBufferedWriter(outputPath, StandardCharsets.UTF_8);) {
+			try {
 				index.toJSON(outputPath);
 
 			} catch (Exception e) {
@@ -126,108 +103,6 @@ public class Driver {
 				System.out.println("There was an error with printing the results");
 			}
 		}
-
-//		
-//		parser.parse(args);
-//
-//		boolean threaded = false;
-//		ThreadedInvertedIndex threadedInvertedIndex = new ThreadedInvertedIndex();
-//		int threads = 5;
-//		if (parser.hasFlag("-threads")) {
-//			threaded = true;
-//
-//			try {
-//
-//				// System.out.println(parser.getString("-threads"));
-//				threads = Integer.parseInt(parser.getValue("-threads"));
-//			} catch (Exception e) {
-//				threads = 5;
-//			}
-//
-//		}
-//		System.out.println("Threads: " + threads);
-//		InvertedIndex index = new InvertedIndex();
-//
-//		if (parser.hasValue("-path")) {
-//			Path inputPath = Paths.get(parser.getString("-path"));
-//
-//			try {
-//				if (threaded) {
-//					ThreadedIndexBuilder.addFiles(inputPath, threadedInvertedIndex, threads);
-//				} else {
-//					InvertedIndexBuilder.addFiles(inputPath, index);
-//				}
-//			} catch (IOException e) {
-//				System.out.println("There was an error building the index");
-//			}
-//
-//		} else {
-//			System.out.println("No path specified, exiting...");
-//
-//		}
-//
-//		if (parser.hasFlag("-index")) {
-//
-//			Path outputPath = parser.getPath("-index", Paths.get("index.json"));
-//			try (BufferedWriter writer = Files.newBufferedWriter(outputPath, StandardCharsets.UTF_8);) {
-//				if (threaded) {
-//					threadedInvertedIndex.toJSON(outputPath);
-//				} else {
-//					index.toJSON(outputPath);
-//				}
-//
-//			} catch (Exception e) {
-//				System.out.println("Error io open file: " + outputPath.toString());
-//			}
-//
-//		}
-//
-//		if (parser.hasFlag("-locations")) {
-//
-//			Path locations = parser.getPath("-locations", Paths.get("locations.json"));
-//
-//			try {
-//				if (threaded) {
-//					threadedInvertedIndex.locationJSON(locations);
-//				} else {
-//					index.locationJSON(locations);
-//				}
-//			} catch (Exception e) {
-//				System.out.println("There was an error retrieving the locations file");
-//			}
-//		}
-//
-//		QueryFileParser query = new QueryFileParser(index);
-//		ThreadedQueryFileParser threadedQueryFileParser = new ThreadedQueryFileParser(threadedInvertedIndex, threads);
-//		if (parser.hasValue("-search")) {
-//			Boolean exact = parser.hasFlag("-exact");
-//			Path queryFile = Paths.get(parser.getString("-search"));
-//			try {
-//
-//				if (threaded) {
-//					threadedQueryFileParser.getQueries(queryFile, exact);
-//				} else {
-//					query.getQueries(queryFile, exact);
-//				}
-//
-//			} catch (Exception e) {
-//				System.out.println("There was an error with your query file");
-//			}
-//		}
-//
-//		if (parser.hasFlag("-results")) {
-//			Path resultsFile = parser.getPath("-results", Paths.get("results.json"));
-//
-//			try {
-//				if (threaded) {
-//					threadedQueryFileParser.printSearch(resultsFile);
-//				} else {
-//					query.printSearch(resultsFile);
-//				}
-//			} catch (Exception e) {
-//				System.out.println("There was an error with printing the results");
-//			}
-//		}
 
 	}
 
